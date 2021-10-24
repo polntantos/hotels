fdd4e34c22789c9b408553110720a669b3cab0fd
polntantos <polykarpos97@gmail.com>
2021-10-24 18:48:48 +0300

    breeze auth added tweet migration model and controller added

---
+++ database/migrations/2021_10_24_152834_create_tweets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTweetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->text('text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
